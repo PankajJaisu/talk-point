98c9c5892b3f666250a0eccdee6c7765518fb391
Pankaj Jaiswal <pankaj@Pankajs-MacBook-Air.local>
2023-07-23 21:52:14 +0530

    added deployment file for server

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -96,7 +96,7 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:4000');
+const socket = io("https://pick-call.onrender.com/");
 // const socket = io("https://warm-wildwood-81069.herokuapp.com");
 
 const ContextProvider = ({ children }) => {
