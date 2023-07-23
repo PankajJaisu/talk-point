7f8b45a5c99fe8de5e502317de7472c1156c038f
Pankaj Jaiswal <pankaj@Pankajs-MacBook-Air.local>
2023-07-23 16:33:49 +0530

    backend done

--- index.js
+++ index.js
@@ -17,5 +17,17 @@ app.get("/",(req,res)=>{
     res.send("Hello World");
 });
 
+io.on('connection',(socket)=>{
+    socket.emit('me',socket.id);
+    socket.on('disconnect',()=>{
+            socket.broadcast.emit('callended');
+        });
+    socket.on('calluser',({userToCall,signalData,from,name})=>{
+        io.to(userToCall).emit('calluser',{signal:signalData,from,name});
+
+    });
+    socket.on("answercall",(data)=>{
+        io.to(data.to).emit("callaccepted",data.signal);
+    })
+})
 server.listen(PORT,()=>console.log(`server listening on port ${PORT}`));
-    
